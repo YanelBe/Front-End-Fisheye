0e94cc280b7f1568744da0c71b162fa4c15fc23a
YanelBe <yanel.bensa@gmail.com>
2023-05-18 13:28:20 +0200

    Assets + Intégration accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,49 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+
+        //Création de l'article qui contiendra les éléments de la bio des photographes
+        const article = document.createElement("article");
+
+        //Création d'un élément h2 pour le nom du photographe
+        const h2 = document.createElement("h2");
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        //Lien vers la page spécifique du photographe en utilisant l'ID
+        const photographerLink = document.createElement("a");
+        const photographerURL = `photographer.html?id=${id}`;
+        photographerLink.setAttribute("href", photographerURL);
+
+        //Création d'un élément h3 pour la location (city, country)
+        const location = document.createElement("h3");
+        location.textContent =  city + ", " + country;
+
+        //Création d'un élément h4 pour la tagline
+        const taglineElement = document.createElement("h4");
+        taglineElement.textContent = tagline
+
+        //Création d'un élément p pour le prix
+        const priceElement = document.createElement("p");
+        priceElement.textContent = price + "€/jour";
+        
+        //Création de l'image en ajoutant un alt avec le nom du photographe
+        const img = document.createElement("img");
+        img.setAttribute("src", picture)
+        img.setAttribute("alt", "Profil photographe de " + name)
+
+        article.appendChild(photographerLink);
+
+        //On utilise appendChild à la varaible photographerLink pour que seul l'image du photographe et son nom soient cliquables
+        photographerLink.appendChild(img);
+        photographerLink.appendChild(h2);
+        
+        article.appendChild(location);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
